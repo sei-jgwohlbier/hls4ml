3a24d8ede08f8740e75f74018a4d553b6ad1ed7f
John Wohlbier <jgwohlbier@sei.cmu.edu>
2024-08-16 10:30:12 -0400

    changes for running exe

--- hls4ml/templates/vivado/build_lib.sh
+++ hls4ml/templates/vivado/build_lib.sh
@@ -2,7 +2,8 @@
 
 CC=g++
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
-    CFLAGS="-O3 -fPIC -std=c++11 -fno-gnu-unique"
+    #CFLAGS="-O3 -fPIC -std=c++11 -fno-gnu-unique"
+    CFLAGS="-O0 -fPIC -std=c++11 -fno-gnu-unique"
 elif [[ "$OSTYPE" == "darwin"* ]]; then
     CFLAGS="-O3 -fPIC -std=c++11"
 fi
@@ -11,7 +12,12 @@ INCFLAGS="-Ifirmware/ap_types/"
 PROJECT=myproject
 LIB_STAMP=mystamp
 
+echo "CC=${CC}"
+echo "CFLAGS=${CFLAGS}"
+echo "INCFLAGS=${INCFLAGS}"
+
 ${CC} ${CFLAGS} ${INCFLAGS} -c firmware/${PROJECT}.cpp -o ${PROJECT}.o
 ${CC} ${CFLAGS} ${INCFLAGS} -c ${PROJECT}_bridge.cpp -o ${PROJECT}_bridge.o
 ${CC} ${CFLAGS} ${INCFLAGS} -shared ${PROJECT}.o ${PROJECT}_bridge.o -o firmware/${PROJECT}-${LIB_STAMP}.so
+${CC} ${CFLAGS} ${INCFLAGS} -o a.out ${PROJECT}_test.cpp firmware/${PROJECT}-${LIB_STAMP}.so
 rm -f *.o
